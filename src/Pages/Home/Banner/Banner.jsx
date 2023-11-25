0676bc1376317da1ffe6c7fdf151f07f6fb46716
iftekharr001 <iftekharr001@gmail.com>
2023-11-25 10:01:10 -0800

    created tastimonial data

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -7,7 +7,7 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay } from "swiper/modules";
 const Banner = () => {
   return (
     <div>
@@ -48,22 +48,36 @@ const Banner = () => {
                   delay: 2500,
                   disableOnInteraction: false,
                 }}
-                pagination={{
-                  clickable: true,
-                }}
-                navigation={true}
-                modules={[Autoplay, Pagination, Navigation]}
+                modules={[Autoplay]}
                 className="mySwiper"
               >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    className="h-[250px] "
+                    src="https://i.ibb.co/HD32dTv/download-removebg-preview-1.png"
+                    alt=""
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    className="h-[250px] "
+                    src="https://i.ibb.co/bQnYMb8/images-removebg-preview.png"
+                    alt=""
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    className="h-[250px] "
+                    src="https://i.ibb.co/DpC7YHB/images-1-removebg-preview.png"
+                    alt=""
+                  />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src="https://i.ibb.co/ZHqLnSG/download-1-removebg-preview.png"
+                    alt=""
+                  />
+                </SwiperSlide>
               </Swiper>
             </div>
           </div>
